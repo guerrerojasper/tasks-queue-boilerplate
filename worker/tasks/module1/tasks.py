57bf764065266acf957d289932fefd3fb55c4b3d
Jasper Joey S. Guerrero <jasperjoey.guerrero@gmail.com>
2025-08-17 08:48:31 +0800

    add name to shared_task for better naming convention per task

--- worker/tasks/module1/tasks.py
+++ worker/tasks/module1/tasks.py
@@ -1,6 +1,6 @@
 from celery import shared_task
 
-@shared_task(queue='queue1', bind=True)
+@shared_task(queue='queue1', name='runModule01', bind=True)
 def task_1(x, y):
     print('task_1')
     return x + y
--- worker/tasks/module2/tasks.py
+++ worker/tasks/module2/tasks.py
@@ -1,6 +1,6 @@
 from celery import shared_task
 
-@shared_task(queue='queue2', bind=True)
+@shared_task(queue='queue2', name='runModule02', bind=True)
 def task_2(x, y):
     print('Task 2')
     return x * y
